f2988bb64527eb83aad84514b93c11b561a6011a
princekatheriya <princekatheriya2@gmail.com>
2023-02-09 11:35:44 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
 
 function App() {
+  const commands = [
+    {
+      command: [
+        "convert primary color to * color",
+        "change colour to *",
+        "change colour to the *",
+      ],
+      callback: (cmdText) => setCommandText(capitalizeFirstLetter(cmdText)),
+    },
+  ];
+
+  const { transcript } = useSpeechRecognition({ commands });
+  const [commandText, setCommandText] = useState("");
+  function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  console.log("commandText =>", capitalizeFirstLetter(commandText));
+  const color = [
+    "Red",
+    "Darkred",
+    "Blue",
+    "Lightblue",
+    "Darkblue",
+    "Orange",
+    "Lightorange",
+    "Darkorange",
+    "Green",
+    "Lightgreen",
+    "Darkgreen",
+    "Yellow",
+    "Lightyellow",
+    "Darkyellow",
+    "Pink",
+    "Lightpink",
+  ];
+
+  if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
+    return null;
+  }
+
+  let paragraph = "";
+
+  if (commandText) {
+    let modifiedCmd = commandText.split(" ").join("");
+    console.log("comd => ", modifiedCmd);
+    if (color.includes(modifiedCmd)) {
+      paragraph = (
+        <p style={{ color: modifiedCmd }}>
+          Colour is Changed to the : {modifiedCmd}
+        </p>
+      );
+    } else {
+      paragraph = <p>Could not find color: {modifiedCmd}</p>;
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div
+        style={{
+          justifyContent: "center",
+          textAlign: "center",
+        }}
+      >
+        <h2>Change color from your voice Command</h2>
+        {paragraph}
+
+        <p id="transcript">Transcript: {transcript}</p>
+
+        <button onClick={SpeechRecognition.startListening}>
+          <span>
+            <i class="fa fa-microphone" aria-hidden="true"></i>
+            <span>...</span>
+          </span>
+        </button>
+      </div>
     </div>
   );
 }
